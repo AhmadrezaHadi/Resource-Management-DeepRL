a3f57b6881cda08674e2596b2f3ae18c4b1f79e7
AhmadrezaHadi <ahmadreza.hadi7@gmail.com>
2022-08-30 16:10:05 +0430

    removed some comments

--- main.py
+++ main.py
@@ -1,25 +1,18 @@
 import gym
-
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.env_checker import check_env
 
 from environment import Env
 from parameters import Parameters
+
 pa = Parameters()
 pa.compute_dependent_parameters()
 
-# Parallel environments
-# env = make_vec_env("CartPole-v1", n_envs=4)
 env = Env(pa, render=False, repre='compact')
 
 model = PPO("MlpPolicy", env, verbose=1)
 model.learn(total_timesteps=100000)
-# model.save("ppo_cartpole")
-
-# del model # remove to demonstrate saving and loading
-
-# model = PPO.load("ppo_cartpole")
 
 env = Env(pa, render=True, repre='compact')
 obs = env.reset()
@@ -31,7 +24,6 @@ while True:
     if done:
         print('done')
         break
-    # env.render()
 
 
 
